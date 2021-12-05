89b57b082f25ab503b869457369b3837918deb41
zeeunkhan <zkhan9715@conestogac.on.ca>
2021-12-04 23:14:14 -0500

    initial commit

---
+++ js/makeit.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var total = [];
+
+const $ = (selector) => document.querySelector(selector);
+
+const collection = document.querySelectorAll("ul img");
+
+// adding hover-over effect to images
+for (var i = 0; i < collection.length; i++) {
+  const image = collection[i];
+  const previousUrl = image.src;
+  const currentUrl = image.id;
+
+  const newImage = new Image();
+  newImage.src = currentUrl;
+
+  image.addEventListener("mouseover", () => {
+    image.src = newImage.src;
+  });
+  image.addEventListener("mouseout", () => {
+    image.src = previousUrl;
+  });
+}
+
+//function to display message after sumitting form (contact page)
+function myFunction() {
+  document.getElementById("submit").innerHTML = "Submitted. Thank You!";
+}
